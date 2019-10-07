8977c48bdaae0bade9c24ef554450e59cd8666c3
gitandlucsil <andlucsil1221@gmail.com>
2019-10-07 20:31:30 -0300

    adicionado bCrypt

--- src/main/java/com/and/luc/sil/api/ApiRestfulSpringBootApplication.java
+++ src/main/java/com/and/luc/sil/api/ApiRestfulSpringBootApplication.java
@@ -3,10 +3,16 @@ package com.and.luc.sil.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.and.luc.sil.security.SenhaUtils;
+
 @SpringBootApplication
 public class ApiRestfulSpringBootApplication {
 
 	public static void main(String[] args) {
+		String senhaEncoded = SenhaUtils.gerarBCrypt("123456");
+		System.out.println(senhaEncoded);
+		boolean senhaValida = SenhaUtils.senhaValida("123456", senhaEncoded);
+		System.out.println(senhaValida);
 		SpringApplication.run(ApiRestfulSpringBootApplication.class, args);
 	}
 
---
+++ src/main/java/com/and/luc/sil/security/SenhaUtils.java
@@ -0,0 +1,29 @@
+package com.and.luc.sil.security;
+
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+
+public class SenhaUtils {
+	/**
+	* Gera um hash utilizando o BCrypt.
+	* @param senha
+	* @return String
+	*/
+	public static String gerarBCrypt(String senha) {
+		if(senha == null) {
+			return senha;
+		}
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.encode(senha);
+	}
+	
+	/**
+	* Verifica se a senha é válida.
+	* @param senha
+	* @param senhaEncoded
+	* @return boolean
+	*/
+	public static boolean senhaValida(String senha, String senhaEncode) {
+		BCryptPasswordEncoder bCryptEncoder = new BCryptPasswordEncoder();
+		return bCryptEncoder.matches(senha, senhaEncode);
+	}
+}
