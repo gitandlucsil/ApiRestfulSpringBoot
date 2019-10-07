88336f5aca45601b877745ff55582ef0b16159e2
gitandlucsil <andlucsil1221@gmail.com>
2019-10-07 20:05:31 -0300

    Arquivos de inicio de projeto

---
+++ src/main/java/com/and/luc/sil/api/ApiRestfulSpringBootApplication.java
@@ -0,0 +1,13 @@
+package com.and.luc.sil.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiRestfulSpringBootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiRestfulSpringBootApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/and/luc/sil/api/ApiRestfulSpringBootApplicationTests.java
@@ -0,0 +1,16 @@
+package com.and.luc.sil.api;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApiRestfulSpringBootApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
